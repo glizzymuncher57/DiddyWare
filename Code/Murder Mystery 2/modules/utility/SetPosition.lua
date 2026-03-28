5756376f38da67f04e85c19beb816c6b53dbea3a
Daniel <dsfxzfu@gmail.com>
2026-03-28 01:21:53 +0000

    Delete Code/Murder Mystery 2/modules directory

--- Code/Murder Mystery 2/modules/utility/SetPosition.lua
+++
@@ -1,5 +0,0 @@
-return function(Instance, NewPosition)
-	for i = 1, 1000 do
-		Instance.Position = NewPosition
-	end
-end
--- Code/Murder Mystery 2/modules/utility/math.lua
+++
@@ -1,9 +0,0 @@
-return function()
-	function math.floor(x)
-		return x - (x % 1)
-	end
-
-	function math.clamp(n, min, max)
-		return math.max(min, math.min(max, n))
-	end
-end
