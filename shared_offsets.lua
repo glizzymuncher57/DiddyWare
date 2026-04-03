8aa9a5df074d22e676d1aedd4c4a47b9c57a808a
Daniel <dsfxzfu@gmail.com>
2026-04-03 11:07:43 +0100

    Create shared_offsets.lua

---
+++ shared_offsets.lua
@@ -0,0 +1,33 @@
+return {
+    ['version'] = 'version-689e359b09ad43b0',
+
+    ['ImageButton'] = {
+        ['Image'] = {Type = 'string', Offset = 0xCC8},
+    },
+    ['Path2D'] = {
+        ['Visible'] = {Type = 'bool', Offset = 0x115},
+    },
+    ['Animator'] = {
+        ['AnimationTrackList'] = {Type = 'pointer', Offset = 0x850},
+    },
+    ['ScreenGui'] = {
+        ['Enabled'] = {Type = 'bool', Offset = 0x4CC},
+    },
+    ['AnimationTrack'] = {
+        ['Speed'] = {Type = 'float', Offset = 0xE4},
+        ['TimePosition'] = {Type = 'float', Offset = 0xE8},
+        ['Looped'] = {Type = 'bool', Offset = 0xF5},
+        ['Animation'] = {Type = 'pointer', Offset = 0xD0},
+    },
+    ['Animation'] = {
+        ['AnimationId'] = {Type = 'string', Offset = 0xD0},
+    },
+    ['GuiObject'] = {
+        ['Size'] = {Type = 'float', X = 0x538, Y = 0x540},
+        ['Position'] = {Type = 'float', X = 0x518, Y = 0x520},
+        ['Visible'] = {Type = 'bool', Offset = 0x5B5},
+        ['AbsolutePosition'] = {Type = 'float', X = 0x110, Y = 0x114},
+        ['Rotation'] = {Type = 'float', Offset = 0x188},
+        ['AbsoluteSize'] = {Type = 'float', X = 0x118, Y = 0x11C},
+    },
+}
