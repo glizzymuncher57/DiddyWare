e813e3bccae1b9632b2352b7a4960cf3722a12d5
Daniel <dsfxzfu@gmail.com>
2026-04-03 09:53:57 +0100

    Delete Code directory

--- Code/Murder Mystery 2/src/modules/utility/GetDistanceSqrt.lua
+++
@@ -1,8 +0,0 @@
-return function(VectorA, VectorB)
-	local dx, dy, dz
-	dx = VectorA.X - VectorB.X
-	dy = VectorA.Y - VectorB.Y
-	dz = VectorA.Z - VectorB.Z
-
-	return math.floor(math.sqrt(dx * dx + dy * dy + dz * dz))
-end
