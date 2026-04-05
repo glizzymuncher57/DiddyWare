09cdb731ff6944d07365ef71481b26587df2deb8
Daniel <dsfxzfu@gmail.com>
2026-04-05 18:07:59 +0100

    Update shared_offsets.lua

--- shared_offsets.lua
+++ shared_offsets.lua
@@ -1,48 +1,44 @@
 return {
     ['version'] = 'version-689e359b09ad43b0',
 
-    ['Path2D'] = {
-        ['Visible'] = {Type = 'bool', Offset = 0x115},
-    },
-    ['ImageButton'] = {
-        ['Image'] = {Type = 'string', Offset = 0xCC8},
-    },
-    ['Animator'] = {
-        ['AnimationTrackList'] = {Type = 'pointer', Offset = 0x850},
-    },
     ['AnimationTrack'] = {
-        ['Animation'] = {Type = 'pointer', Offset = 0xD0},
-        ['Speed'] = {Type = 'float', Offset = 0xE4},
-        ['TimePosition'] = {Type = 'float', Offset = 0xE8},
         ['Looped'] = {Type = 'bool', Offset = 0xF5},
+        ['TimePosition'] = {Type = 'float', Offset = 0xE8},
+        ['Speed'] = {Type = 'float', Offset = 0xE4},
+        ['Animation'] = {Type = 'pointer', Offset = 0xD0},
     },
     ['Animation'] = {
         ['AnimationId'] = {Type = 'string', Offset = 0xD0},
     },
+    ['GuiObject'] = {
+        ['Rotation'] = {Type = 'float', Offset = 0x188},
+        ['Position'] = {Type = 'float', X = 0x518, Y = 0x520},
+        ['Visible'] = {Type = 'bool', Offset = 0x5B5},
+        ['AbsolutePosition'] = {Type = 'float', X = 0x110, Y = 0x114},
+        ['AbsoluteSize'] = {Type = 'float', X = 0x118, Y = 0x11C},
+        ['Size'] = {Type = 'float', X = 0x538, Y = 0x540},
+    },
     ['ScreenGui'] = {
         ['Enabled'] = {Type = 'bool', Offset = 0x4CC},
     },
-    ['Primitive'] = {
-        ['Position'] = {Type = 'float', X = 0xE4, Y = 0xE8, Z = 0xEC},
-        ['Rotation'] = {Type = 'float', Offset = 0xC0},
-    },
-    ['SphereHandleAdornment'] = {
-        ['Position'] = {Type = 'float', X = 0x154, Y = 0x158, Z = 0x15C},
-        ['Transparency'] = {Type = 'float', Offset = 0xFC},
-    },
-    ['CylinderHandleAdornment'] = {
-        ['Position'] = {Type = 'float', X = 0x154, Y = 0x158, Z = 0x15C},
-        ['Transparency'] = {Type = 'float', Offset = 0xFC},
-    },
-    ['GuiObject'] = {
-        ['Visible'] = {Type = 'bool', Offset = 0x5B5},
-        ['AbsolutePosition'] = {Type = 'float', X = 0x110, Y = 0x114},
-        ['Rotation'] = {Type = 'float', Offset = 0x188},
-        ['AbsoluteSize'] = {Type = 'float', X = 0x118, Y = 0x11C},
-        ['Position'] = {Type = 'float', X = 0x518, Y = 0x520},
-        ['Size'] = {Type = 'float', X = 0x538, Y = 0x540},
+    ['Path2D'] = {
+        ['Visible'] = {Type = 'bool', Offset = 0x115},
     },
     ['BasePart'] = {
-        ['Primitive'] = {Type = 'pointer', Offset = 0x148},
+        ['Primitive'] = {Type = 'pointer', Offset = 0x8},
+    },
+    ['Primitive'] = {
+        ['Rotation'] = {Type = 'float', Offset = 0xF38},
+    },
+    ['Animator'] = {
+        ['AnimationTrackList'] = {Type = 'pointer', Offset = 0x850},
+    },
+    ['CylinderHandleAdornment'] = {
+        ['Rotation'] = {Type = 'float', Offset = 0x130},
+        ['Transparency'] = {Type = 'float', Offset = 0xFC},
+        ['Height'] = {Type = 'float', Offset = 0x1C4},
+    },
+    ['ImageButton'] = {
+        ['Image'] = {Type = 'string', Offset = 0xCC8},
     },
 }
